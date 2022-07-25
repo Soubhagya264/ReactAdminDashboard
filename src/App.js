f28262c02d17c9604d440b9846599ad362526dd2
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2022-07-25 14:34:08 +0530

    UserList and userupdate  created

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import './App.css';
 import Topbar from './components/Topbar/Topbar';
 import SideBar from './components/Sidebar/Sidebar';
 import UserList from './Pages/UserList/UserList';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NewUser from './Pages/newUser/NewUser';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import User from './Pages/User/User';
 import Home from './Pages/home';
 
 function App() {
   return (
     <>
-    <Topbar />
-    <div className="container">
-    <SideBar />
-    <div className="others">
       <Router>
-        <Routes>
-       
-          <Route path="/" element={<Home/>} />
-          <Route path="/users" element={<UserList/>} />
-        
-        </Routes>
+        <Topbar />
+        <div className="container">
+          <SideBar />
+          <div className="others">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/users" element={<UserList />} />
+              <Route path="/user/:id" element={<User />} />
+              <Route path="/newUser" element={<NewUser />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
-      </div>
-      </div>
-      
-
     </>
   );
 }
